be7bd81d77769b5cfc211c49a10e4d321f758923
zhangchengcheng <zhangchengcheng@seentao.com>
2026-08-24 19:04:09 +0800

    bug: orbit-notebook__009 baseline (R1)

---
+++ internal/codec/json.go
@@ -0,0 +1,28 @@
+package codec
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func ReadJSON(path string, target any) error {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(data, target); err != nil {
+		return fmt.Errorf("decode %s: %w", filepath.Base(path), err)
+	}
+	return nil
+}
+func WriteJSON(path string, value any) error {
+	data, err := json.MarshalIndent(value, "", "  ")
+	if err != nil {
+		return err
+	}
+	data = append(data, '\n')
+	return os.WriteFile(path, data, 0644)
+}
+func Encode(value any) ([]byte, error) { return json.Marshal(value) }
